c1757459ed04ce7cfa8b6a9e9a62af5ed982bf74
Guliveer <oliwer.pawelski@gmail.com>
2023-01-18 23:56:53 +0100

    Main script improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
+async function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 setInterval(function () {
 	window.scrollTo(0, 0);
 
 	var img = $("#DisplaySrc, #BgSrc");
 	var imgNum = Math.ceil(Math.random() * imgCount);
-	console.log(imgNum);
 	var imgSrc = "ed_paznokcie/pic (" + imgNum + ").jpg";
 
-	img.fadeOut(1000, function () {
+	img.fadeOut(1000, async function () {
 		img.attr("src", imgSrc);
+		await sleep(100);
 		img.fadeIn(1000);
 	});
 }, 15000);
