64647e96997cbd16b0796483503712853a91114f
Guliveer <oliwer.pawelski@gmail.com>
2023-01-20 11:57:06 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ async function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-setInterval(function () {
+setInterval(async function () {
 	window.scrollTo(0, 0);
 
 	var img = $("#DisplaySrc, #BgSrc");
 	var imgNum = Math.ceil(Math.random() * imgCount);
-	var imgSrc = "ed_paznokcie/pic (" + imgNum + ").jpg";
+	var imgSrc = "pictures/pic (" + imgNum + ").jpg";
 
 	img.fadeOut(1000, async function () {
 		img.attr("src", imgSrc);
--- download.cmd
+++ download.cmd
@@ -2,20 +2,21 @@
 cd /d %~dp0
 
 set profile="ed_paznokcie"
+set folder_name="pictures"
 
 cls && set "params=%*" && cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-if not exist %profile% mkdir %profile%
-cd %profile%
+if not exist %folder_name% mkdir %folder_name%
+cd %folder_name%
 del /q /f *
 cd ..
 
-call instaloader %profile%
-cd %profile%
+call instaloader %profile% --dirname-pattern %folder_name% --no-resume --no-profile-pic --no-videos --no-captions --no-metadata-json --no-compress-json 
+cd %folder_name%
 del /q /f /a:-d *.txt
 del /q /f /a:-d *.json*
 del /q /f /a:-d *.mp4
-del /q /f /a:-d id
+del /q /f /a:-d *id
 del /q /f /a:-d *profile_pic.jpg
 
 echo Renaming files...
