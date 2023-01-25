efe2e4ffafe7b6cb8e5dc084acf29b60ea4ac567
Guliveer <oliwer.pawelski@gmail.com>
2023-01-25 20:19:41 +0100

    Update script.js
    Fixed image changing script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,20 @@ async function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-var lastImg = 0; // Declare a variable for later use
+var lastImg = 0; // Declare a variable for later use (contains the last image number)
 
 // Interval for the images change
 setInterval(async function () {
-	window.scrollTo(0, 0);
+	window.scrollTo(0, 0); // Scroll to the top of the page (just in case)
 
 	var img = $("#DisplaySrc, #BgSrc"); // Set the image destination
-	var imgNum = Math.ceil(Math.random() * imgCount); // Get a random image number between 1 and the number of images (from imgCount.js)
 
-	// If the image is the same as the last one, pick a new one
-	while (imgNum == lastImg) {
-		imgNum = Math.ceil(Math.random() * imgCount);
-	}
+	// Pick a new image that's different from the last one
+	do {
+		imgNum = Math.ceil(Math.random() * imgCount); // Get a random image number between 1 and the number of images (imgCount -> from imgCount.js)
+	} while (imgNum == lastImg); // Repeat until the image number is different from the last one
+
+	lastImg = imgNum; // Set the last image to the current one
 
 	var imgSrc = "pictures/pic (" + imgNum + ").jpg"; // Set the name of the source image
 
